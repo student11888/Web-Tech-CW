5b65603adc9a6094bdbdcc282761ae91d64c9264
Farrukh Ruzmetov <wiut00011888@gmail.com>
2021-11-21 23:55:50 +0500

    Mistakes in HTML fixed

--- script.js
+++ script.js
@@ -80,6 +80,33 @@ if (document.URL.includes('contact.html')) {
 
 // Register page
 if (document.URL.includes('register.html')) {
+  const btnSignIn = document.querySelectorAll('.btn-sign-in');
+  const btnSignUp = document.querySelectorAll('.btn-sign-up');
+  const signInPhoto = document.querySelector('.sign-in-photo');
+  const signInInput = document.querySelector('.sign-in-input');
+  const signUpPhoto = document.querySelector('.sign-up-photo');
+  const signUpInput = document.querySelector('.sign-up-input');
+
+  btnSignUp.forEach((b) =>
+    b.addEventListener('click', function () {
+      signInInput.style.transform = 'translateY(-100vh)';
+      signUpPhoto.style.transform = 'translateY(-70vh)';
+
+      signInPhoto.style.transform = 'translateY(100vh)';
+      signUpInput.style.transform = 'translateY(-70vh)';
+    })
+  );
+
+  btnSignIn.forEach((b) =>
+    b.addEventListener('click', function () {
+      signInInput.style.transform = 'translateY(0)';
+      signUpPhoto.style.transform = 'translateY(0)';
+
+      signInPhoto.style.transform = 'translateY(0)';
+      signUpInput.style.transform = 'translateY(-140vh)';
+    })
+  );
+  // Register form validation
   const registerForm = document.querySelector('.register-form');
   registerForm.addEventListener('submit', (e) => {
     e.preventDefault();
